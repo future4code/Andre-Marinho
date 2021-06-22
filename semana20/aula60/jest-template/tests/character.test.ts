8cb7b2e48e22369df37d4b4c6809d6f8b466016b
andremuniz92 <andre_luis_92@hotmail.com>
2021-06-22 18:52:44 -0300

    exercicio completo

--- semana20/aula60/jest-template/tests/character.test.ts
+++ semana20/aula60/jest-template/tests/character.test.ts
@@ -1,4 +1,4 @@
-import { validateCharacter } from "../src/character"
+import { Character, performAttack, validateCharacter } from "../src/character"
 
 describe("Unitary tests", ()=> {
 
@@ -77,6 +77,63 @@ describe("Unitary tests", ()=> {
     test("Creating Mocks", () => {
         const validatorMock = jest.fn(() => {
                 return false
-            });
-    });
+            })
+    })
+
+    test("Should perform attack", () => {
+        const validatorMock = jest.fn(() => {
+            return true
+        })
+
+        const attacker: Character = {
+            name: "Dedé",
+            life: 1500,
+            defense: 1200,
+            strength: 1600
+        }
+
+        const defender: Character = {
+            name: "Vicente",
+            life: 1500,
+            defense: 1400,
+            strength: 1800,
+        }
+
+        performAttack(attacker, defender, validatorMock as any)
+
+        expect(defender.life).toEqual(1300)
+        expect(validatorMock).toHaveBeenCalled()
+        expect(validatorMock).toHaveBeenCalledTimes(2)
+        expect(validatorMock).toHaveReturnedTimes(2)
+    })
+
+    test("Should return a invalid character error", () => {
+        expect.assertions(4)
+        const validatorMock = jest.fn(()=> {
+            return false
+        })
+
+        const attacker: Character = {
+            name: "Dedé",
+            life: 0,
+            defense: 1200,
+            strength: 1600,
+        }
+
+        const defender: Character = {
+            name: "capeta",
+            life: 1500,
+            defense: 1400,
+            strength: 1800,
+        }
+
+        try {
+            performAttack(attacker, defender, validatorMock as any)
+        } catch (error) {
+            expect(error.message).toBe("Invalid character")
+            expect(validatorMock).toHaveBeenCalled()
+            expect(validatorMock).toHaveBeenCalledTimes(1)
+            expect(validatorMock).toHaveReturnedTimes(1)
+        }
+    })
 })
